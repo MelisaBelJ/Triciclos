3a6031578bd9bb32e9dcf670f2f1526637fbc595
Melisa <123397651+MelisaBelJ@users.noreply.github.com>
2023-04-25 19:59:48 +0200

    Update Triciclos.py

--- Triciclos.py
+++ Triciclos.py
@@ -6,6 +6,53 @@ class Triciclos():
         pass
     
     @staticmethod
+    def getBordes(line):
+        a = line.split(",")
+        if a[0] == a[1]:
+            return None
+        else:
+            r = a[0] < a[1]
+            return a[(r+1)%2], a[r]
+        
+    @staticmethod
+    def transforma(fila):
+        x, lista = fila[0], list(fila[1])
+        r = []
+        for y in lista:
+            if x != y:
+                e = (x, y), 'existe'
+                if e not in r:
+                    r.append(e)
+        for a in combinations(lista, 2):
+            if a[0] != a[1]:
+                z = a[0] < a[1]:
+                e = (a[(z+1)%2], a[z]), ('pendiente', x)
+                if e not in r:
+                    r.append(e)
+        return r
+
+    @staticmethod
+    def hayExiste(fila):
+        _, valores = fila
+        x = False
+        for valor in valores:
+            if valor == "existe":
+                x = True
+                break
+        return x
+
+    @staticmethod
+    def encuentraTriciclos(fila):
+        clave, valores = fila
+        bordes = []
+        for valor in valores:
+            if valor != "existe":
+                _, a1 = valor
+                a2, a3 = clave
+                bordes.append((a1, a2, a3))
+        return bordes
+    
+    @staticmethod
     def getTriciclos(ficheros, local):
         if type(ficheros) is list:
             if local:
@@ -22,8 +69,12 @@ class Triciclos():
     @staticmethod
     def getTriciclosMult (ficheros):
         x = 0
-        for fichero in ficheros:
-            pass
+        with SparkContext() as sc:
+            for fichero in ficheros:
+                data = sc.textFile(fname)
+                rdd = data.map(Triciclos.getBordes).filter(lambda x: x != None).groupByKey().\
+                flatMap(Triciclos.transforma).groupByKey().filter(Triciclos.hayExiste).flatMap(Triciclos.encuentraTriciclos)
+                x = rdd.collect()
         return f'En {ficheros} hay {x} triciclos'
     
     @staticmethod
